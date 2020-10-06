5e07914ac08a5e51dd9007c9478043d2c8fc8c4b
MattLWatts <47779633+MattLWatts@users.noreply.github.com>
2020-10-06 12:00:04 +0100

    InitialFiles
    - Added base movement script
    - Added sensor script
       - Ray bundle not fully working yet
    - Added tester scene

---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    // Character speed
+    public float speed = 10.0f;
+    // Rotation speeds
+    public float rotationSpeed = 200.0f;
+    public float autoRotationSpeed = 2.0f;
+    // Object to seek
+    public GameObject findObject;
+
+    // AutoMove to seek object
+    private bool autoMode = false;
+
+    // Returns cross product of 2 vectors
+    Vector3 Cross(Vector3 a, Vector3 b)
+    {
+
+        Vector3 crossProd = new Vector3(0, 0, 0);
+
+        crossProd.x = (a.y * b.z) - (a.z * b.y);
+        crossProd.y = (a.z * b.x) - (a.x * b.z);
+        crossProd.z = (a.x * b.y) - (a.y * b.x);
+
+        return crossProd;
+    }
+
+    // Calculate the vector to the seek object
+    float CalculateAngle()
+    {
+
+        // Vector to the seek object
+        Vector3 vectorToFindObject = findObject.transform.position - this.transform.position;
+
+        // Dot product calculation
+
+        float dot = (this.transform.forward.x * vectorToFindObject.x) + (this.transform.forward.y * vectorToFindObject.y) + (this.transform.forward.z * vectorToFindObject.z);
+
+        // Calculate angle between objects
+
+        float lengthA = Mathf.Sqrt((this.transform.forward.x * this.transform.forward.x) + (this.transform.forward.y * this.transform.forward.y) + (this.transform.forward.z * this.transform.forward.z));
+        float lengthB = Mathf.Sqrt((vectorToFindObject.x * vectorToFindObject.x) + (vectorToFindObject.y * vectorToFindObject.y) + (vectorToFindObject.z * vectorToFindObject.z));
+
+        float angle = Mathf.Acos(angle = dot / (lengthA * lengthB)) * 180 / Mathf.PI;
+
+        // Gets cross product
+
+        Vector3 crossProd = Cross(this.transform.forward, vectorToFindObject);
+
+        // Checks if positive
+
+        if (crossProd.y < 0)
+        {
+            angle *= -1;
+        }
+
+        // Forward facing ray
+        Debug.DrawRay(this.transform.position, this.transform.forward * 10.0f, Color.green, 2.0f);
+        // Ray to finder object
+        Debug.DrawRay(this.transform.position, vectorToFindObject, Color.red, 2.0f);
+
+        return angle;
+    }
+
+    // Calculate the distance from current position to finder object
+    float CalculateDistance()
+    {
+        // Calculate the distance between the objects using pythagoras
+
+        float distance = Mathf.Sqrt(((this.transform.position.x - findObject.transform.position.x) * (this.transform.position.x - findObject.transform.position.x)) + ((this.transform.position.z - findObject.transform.position.z) * (this.transform.position.z - findObject.transform.position.z)));
+
+        return distance;
+    }
+
+    void AutonomousMode()
+    {
+        // Get distance and angle
+        float distance = CalculateDistance();
+        float angle_to_turn = CalculateAngle();
+
+        // Rotate towards object
+
+        this.transform.Rotate(0, angle_to_turn * Time.deltaTime * distance / 20 * autoRotationSpeed, 0);
+
+        // Translate locally forward in z
+        this.transform.Translate(new Vector3(0, 0, speed * Time.deltaTime), Space.Self);
+    }
+
+    void Update()
+    {
+        // Get the horizontal and vertical axis.
+        // By default they are mapped to the arrow keys.
+        // The value is in the range -1 to 1
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        // Move translation along the object's z-axis
+        transform.Translate(0, 0, translation);
+
+        // Rotate around our y-axis
+        transform.Rotate(0, rotation, 0);
+
+        // Check if autonomous
+        if (autoMode == true)
+        {
+            AutonomousMode();
+        }
+
+        // Check for the spacebar being pressed
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Call CalculateAngle method
+            CalculateAngle();
+        }
+
+        // Flip autoMode flag
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            autoMode = !autoMode;
+        }
+    }
+}
---
+++ Assets/Scripts/SensorScript.cs
@@ -0,0 +1,195 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEditor.PackageManager;
+using UnityEngine;
+
+public class SensorScript : MonoBehaviour
+{
+
+    public LayerMask hitMask;
+    public enum Type
+    {
+        Line,
+        BoxCheck,
+        SphereCheck,
+        RayBundle,
+        SphereCast,
+        BoxCast,
+    }
+
+    public Type sensorType = Type.Line;
+    public float raycastLength = 1.0f;
+    [Header("BoxExtent Settings")]
+    public Vector3 boxExtents = new Vector3(1.0f, 1.0f, 1.0f);
+    [Header("SphereCast Settings")]
+    public float spherecastRadius = 5.0f;
+    [Header("RayBundle Settings")]
+    [Range(1, 20)]
+    public int rayResolution;
+    [Range(1, 360)]
+    public int arcLength;
+
+    Transform cachedTransform;
+    public bool Hit { get; private set; }
+    public RaycastHit info = new RaycastHit();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cachedTransform = GetComponent<Transform>();
+        arcLength = 1;
+        rayResolution = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Scan();
+    }
+
+    public bool Scan()
+    {
+        Hit = false;
+        Vector3 dir = cachedTransform.forward;
+        switch (sensorType)
+        {
+            case Type.Line:
+                if (Physics.Linecast(cachedTransform.position, cachedTransform.position + dir * raycastLength, out info, hitMask, QueryTriggerInteraction.Ignore))
+                {
+                    Hit = true;
+                    return true;
+                }
+                break;
+            case Type.BoxCheck:
+                if (Physics.CheckBox(this.transform.position, boxExtents/2, this.transform.rotation, hitMask, QueryTriggerInteraction.Ignore))
+                {
+                    Hit = true;
+                    return true;
+                }
+                break;
+            case Type.SphereCheck:
+                if (Physics.CheckSphere(this.transform.position, spherecastRadius , hitMask, QueryTriggerInteraction.Ignore))
+                {
+                    Hit = true;
+                    return true;
+                }
+                break;
+            case Type.RayBundle:
+                float angleInbetween = arcLength / rayResolution;
+                dir.y = dir.y - arcLength / 2;
+                for (int i = 0; i < rayResolution; i++)
+                {
+                    if (Physics.Linecast(cachedTransform.position, cachedTransform.position + dir * raycastLength, out info, hitMask, QueryTriggerInteraction.Ignore))
+                    {
+                        Hit = true;
+                        return true;
+                    }
+                    dir.y += angleInbetween;
+                }
+                break;
+            case Type.SphereCast:
+                if (Physics.SphereCast(new Ray(cachedTransform.position, dir), spherecastRadius, out info, raycastLength, hitMask, QueryTriggerInteraction.Ignore))
+                {
+                    Hit = true;
+                    return true;
+                }
+                if (Physics.CheckSphere(this.transform.position, spherecastRadius, hitMask, QueryTriggerInteraction.Ignore))
+                {
+                    Hit = true;
+                    return true;
+                }
+                break;
+            case Type.BoxCast:
+                if (Physics.BoxCast(cachedTransform.position, boxExtents/2, dir, cachedTransform.rotation,raycastLength, hitMask, QueryTriggerInteraction.Ignore))
+                {
+                    Hit = true;
+                    return true;
+                }
+                if (Physics.CheckBox(this.transform.position, boxExtents / 2, this.transform.rotation, hitMask, QueryTriggerInteraction.Ignore))
+                {
+                    Hit = true;
+                    return true;
+                }
+                break;
+        }
+        return false;
+    }
+
+    Vector3 VectorRotate(Vector3 currentPos, Vector3 vectorToRotate, float rotationAngle)
+    {
+        Vector3 direction = vectorToRotate - currentPos;
+        direction = Quaternion.Euler(0, rotationAngle, 0) * direction;
+        vectorToRotate = direction + currentPos;
+        return vectorToRotate;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        if (cachedTransform == null)
+        {
+            cachedTransform = GetComponent<Transform>();
+        }
+        Scan();
+        if (Hit)
+        {
+            Gizmos.color = Color.red;
+        }
+        Gizmos.matrix *= Matrix4x4.TRS(transform.position, transform.rotation, Vector3.one);
+        float length = raycastLength;
+        switch (sensorType)
+        {
+            case Type.Line:
+                if (Hit)
+                {
+                    length = Vector3.Distance(this.transform.position, info.point);
+                }
+                Gizmos.DrawLine(Vector3.zero, Vector3.forward * length);
+                Gizmos.color = Color.green;
+                Gizmos.DrawCube(Vector3.forward * length, new Vector3(0.02f, 0.02f, 0.02f));
+                break;
+            case Type.BoxCheck:
+                Gizmos.DrawWireCube(Vector3.zero, boxExtents);
+                break;
+            case Type.SphereCheck:
+                Gizmos.DrawWireSphere(Vector3.zero, spherecastRadius);
+                break;
+            case Type.RayBundle:
+                float angleInbetween = 0;
+                float rotation = 0;
+                if (rayResolution > 1)
+                {
+                    angleInbetween = arcLength / rayResolution;
+                    rotation = -arcLength / 2;
+                }
+                for (int i = 0; i < (rayResolution); i++)
+                {
+                    rotation += angleInbetween;
+                    Gizmos.DrawLine(Vector3.zero, VectorRotate(Vector3.zero, Vector3.forward, rotation) * length);
+                }
+                break;
+            case Type.SphereCast:
+                Gizmos.DrawWireSphere(Vector3.zero, spherecastRadius);
+
+                Gizmos.DrawLine(Vector3.up * spherecastRadius, Vector3.up * spherecastRadius + Vector3.forward * length);
+                Gizmos.DrawLine(Vector3.down * spherecastRadius, Vector3.down * spherecastRadius + Vector3.forward * length);
+                Gizmos.DrawLine(Vector3.left * spherecastRadius, Vector3.left * spherecastRadius + Vector3.forward * length);
+                Gizmos.DrawLine(Vector3.right * spherecastRadius, Vector3.right * spherecastRadius + Vector3.forward * length);
+
+                Gizmos.DrawWireSphere(Vector3.zero + Vector3.forward * length, spherecastRadius);
+                break;
+            case Type.BoxCast:
+                Gizmos.DrawWireCube(Vector3.zero, boxExtents);
+
+                Gizmos.DrawLine(new Vector3(-boxExtents.x, boxExtents.y, 0) / 2, new Vector3(-boxExtents.x, boxExtents.y, 0) / 2 + Vector3.forward * length);
+                Gizmos.DrawLine(new Vector3(-boxExtents.x, -boxExtents.y, 0) / 2, new Vector3(-boxExtents.x, -boxExtents.y, 0) / 2 + Vector3.forward * length);
+                Gizmos.DrawLine(new Vector3(boxExtents.x, boxExtents.y, 0) / 2, new Vector3(boxExtents.x, boxExtents.y, 0) / 2 + Vector3.forward * length);
+                Gizmos.DrawLine(new Vector3(boxExtents.x, -boxExtents.y, 0) / 2, new Vector3(boxExtents.x, -boxExtents.y, 0) / 2 + Vector3.forward * length);
+
+                Gizmos.DrawWireCube(Vector3.zero + Vector3.forward * length, boxExtents);
+                break;
+        }
+    }
+
+}
