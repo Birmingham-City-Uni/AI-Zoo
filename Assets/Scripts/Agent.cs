4129af5a2b090dec7a5e420bcae4ecbae6f2d20e
MattLWatts <47779633+MattLWatts@users.noreply.github.com>
2020-10-11 16:41:06 +0100

    Updated Agent script
    - Added comments
    - Split out state selection into seperate function
    - Added current state enum

--- Assets/Scripts/Agent.cs
+++ Assets/Scripts/Agent.cs
@@ -1,14 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Transactions;
 using UnityEngine;
+
 
 public class Agent : MonoBehaviour
 {
+    // Creates instances of sensor and FSM 
     FSMStateManager sm = new FSMStateManager();
     SensorScript sensor = new SensorScript();
 
-    bool stateChange = false;
+    // States used by agent
+    private enum State
+    {
+        Idle,
+        Sensing,
+    }
 
+    State currentState = State.Idle;
+
+    // Default in-editor settings for sense script
     public SensorScript.SensorType sensorType = SensorScript.SensorType.Line;
     public float raycastLength = 1.0f;
     [Header("BoxExtent Settings")]
@@ -21,9 +32,11 @@ public class Agent : MonoBehaviour
     [Range(1, 360)]
     public int arcLength;
 
+
     // Start is called before the first frame update
     void Start()
     {
+        // Gives sensor scripts the in-editor variables set above
         sensor.sensorType = this.sensorType;
         sensor.boxExtents = this.boxExtents;
         sensor.raycastLength = this.raycastLength;
@@ -31,22 +44,33 @@ public class Agent : MonoBehaviour
         sensor.rayResolution = this.rayResolution;
         sensor.arcLength = this.arcLength;
 
+        // Sets state to seek
         sm.ChangeState(new SeekState(this, sensor));
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Calls state update
         sm.Update();
-        if (sensor.Hit == true && stateChange == true)
+        // Calls state selector
+        StateSelector();
+    }
+
+    void StateSelector()
+    {
+        // Checks for sensor hit and if state is not currently idle
+        if (sensor.Hit == true && currentState != State.Idle)
         {
             sm.ChangeState(new IdleState(this));
-            stateChange = !stateChange;
+            // Changes enum to current state
+            currentState = State.Idle;
         }
-        if (sensor.Hit == false && stateChange == false)
+
+        if (sensor.Hit == false && currentState != State.Sensing)
         {
             sm.ChangeState(new SeekState(this, sensor));
-            stateChange = !stateChange;
+            currentState = State.Sensing;
         }
     }
 }
