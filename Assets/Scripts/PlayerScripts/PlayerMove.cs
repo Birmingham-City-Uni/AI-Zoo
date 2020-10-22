dda8a47132e06d8a493ad125e28bd2a4ab5bb60b
MattLWatts <47779633+MattLWatts@users.noreply.github.com>
2020-10-22 17:03:04 +0100

    Development (#2)
    * Sprint 001
    
    - Added 3D pathfinder script with Node and Grid files
    - Needs testing before sprint closure
    
    * Sprint 002
    
    - Basic scene created
    - Movement and Camera look scripts created

---
+++ Assets/Scripts/PlayerScripts/PlayerMove.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    // Player speed
+    public float speed = 2.0f;
+
+    private bool canMove;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canMove = true;
+    }
+
+    private void Move()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.back * speed * Time.deltaTime, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime, Space.Self);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canMove == true)
+        {
+            Move();
+        }
+    }
+}
