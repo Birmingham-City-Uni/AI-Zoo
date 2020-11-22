5f132eb3b7e2b6ce081a5476deba5cf760e0b589
MattLWatts <47779633+MattLWatts@users.noreply.github.com>
2020-11-22 16:00:44 +0000

    Sprint 008
    Initial boids implementation. Fixed with no errors

---
+++ Assets/Scripts/SteeringBehaviours/Boids/BoidEntity.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidEntity : MonoBehaviour
+{
+
+    private GameObject player;
+
+    private Vector3 playerPosition;
+
+    [SerializeField]
+    private float rotationSpeed = 3.0f;
+
+    private float speed = 1.0f;
+
+    private float distanceToNeighbour = 10.0f;
+    private bool turning = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Random.Range(0.5f, 2.0f);
+        player = GameObject.FindWithTag("Player");
+        playerPosition = player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Update player position so that boids goal moves with player
+        playerPosition = player.transform.position;
+
+        if (Vector3.Distance(this.transform.position, playerPosition) >= FlockSpawn.flockBounds)
+        {
+            turning = true;
+        }
+        else if (this.transform.position.y <= 10.0f)
+        {
+            turning = true;
+        }
+        else
+        {
+            turning = false;
+        }
+
+        if (turning == true)
+        {
+            Vector3 direction = playerPosition + new Vector3(0, Random.Range(10f, FlockSpawn.flockBounds), 0) - this.transform.position;
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), rotationSpeed * Time.deltaTime);
+            speed = Random.Range(0.5f, 3.0f);
+        }
+
+        if (Random.Range(0,5) < 1  && turning != true)
+        {
+            GameObject[] flockBirds;
+            flockBirds = FlockSpawn.birdsArray;
+
+            Vector3 centerPosition = this.transform.position;
+            Vector3 avoidPosition = playerPosition;
+
+            float flockSpeed = 0.5f;
+            Vector3 goalPos = FlockSpawn.currentGoalPosition;
+
+            float distance;
+            int neighbourGroupSize = 0;
+
+            foreach (GameObject bird in flockBirds)
+            {
+                if (bird != this.gameObject)
+                {
+                    distance = Vector3.Distance(bird.transform.position, this.transform.position);
+                    if (distance <= distanceToNeighbour)
+                    {
+                        centerPosition += bird.transform.position;
+                        neighbourGroupSize += 1;
+
+                        if (distance < 6.0f)
+                        {
+                            avoidPosition = avoidPosition + (this.transform.position - bird.transform.position);
+                        }
+
+                        BoidEntity AnotherFlockEntity = bird.GetComponent<BoidEntity>();
+                        flockSpeed += AnotherFlockEntity.speed;
+                    }
+                }
+            }
+
+            if (neighbourGroupSize > 0)
+            {
+                centerPosition = centerPosition / neighbourGroupSize + (goalPos - this.transform.position);
+
+                speed = (flockSpeed / neighbourGroupSize) + Random.Range(-0.1f, 0.1f);
+                if (speed > 5.0f)
+                {
+                    speed = Random.Range(0.5f, 3.0f);
+                }
+
+                Vector3 direction = (centerPosition + avoidPosition) - this.transform.position;
+
+                this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), rotationSpeed * Time.deltaTime);
+            }
+        }
+        transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/SteeringBehaviours/Boids/FlockSpawn.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class FlockSpawn : MonoBehaviour
+{
+
+    // Bird object
+    public GameObject birdPrefab;
+
+    // Size of bounding box that dictates both boid spawn position, and movement bounds
+    public static int flockBounds = 100;
+    // Amount of boids that are spawned
+    private static int flockSize = 700;
+
+    public static GameObject[] birdsArray = new GameObject[flockSize];
+
+    public GameObject goalPositionParentNode;
+
+    private GameObject[] goalPositionsArray;
+
+    public static Vector3 currentGoalPosition;
+    private Vector3 birdSpawnPosition;
+    private int currentGoalIndex;
+
+    public GameObject player;
+
+    private Vector3 playerPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int arraySize = goalPositionParentNode.transform.childCount;
+        goalPositionsArray = new GameObject[arraySize];
+
+        for (int i = 0;  i < arraySize;  i++)
+        {
+            goalPositionsArray[i] = goalPositionParentNode.transform.GetChild(i).gameObject;
+        }
+
+        currentGoalIndex = Random.Range(0, arraySize);
+        playerPosition = player.transform.position;
+        currentGoalPosition = goalPositionsArray[currentGoalIndex].transform.position;
+
+        SpawnBirds();
+    }
+
+    private void Update()
+    {
+        // Update player position to keep gizmos updating
+        playerPosition = player.transform.position;
+    }
+
+    void SpawnBirds()
+    {
+        for (int i = 0; i < flockSize; i++)
+        {
+            birdSpawnPosition = new Vector3(Random.Range(playerPosition.x - flockBounds, playerPosition.x + flockBounds),
+                                            Random.Range(playerPosition.y + 10f, playerPosition.y + flockBounds),
+                                            Random.Range(playerPosition.z - flockBounds, playerPosition.z + flockBounds));
+            // Create birds at random position within bounds with differing rotations
+            birdsArray[i] = Instantiate(birdPrefab, birdSpawnPosition, Quaternion.LookRotation(new Vector3(Random.Range(-1.0f, 1.0f), 0, Random.Range(-1.0f, 1.0f))));
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        // Display boid bounds in the scene
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireCube(new Vector3(playerPosition.x, playerPosition.y + 55, playerPosition.z), new Vector3(flockBounds * 2, 90, flockBounds * 2));
+    }
+}
